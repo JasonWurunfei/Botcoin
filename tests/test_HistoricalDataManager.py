d9f6b84ceb8965c92c38bb1de646ab46da33d41b
Runfei Wu <47469275+JasonWurunfei@users.noreply.github.com>
2025-04-18 11:52:46 +0200

    Add necessary dependencies in requirements.txt

--- tests/test_HistoricalDataManager.py
+++ tests/test_HistoricalDataManager.py
@@ -4,7 +4,6 @@ import sys
 import os
 import pandas as pd
 from datetime import datetime
-import pytz
 
 # Add src to path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))

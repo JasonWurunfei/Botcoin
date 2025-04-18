f5da92a034b0aadbe5c28f9d27a8ba414c6c648f
Runfei Wu <47469275+JasonWurunfei@users.noreply.github.com>
2025-04-18 12:01:03 +0200

    add plotly to requirements.txt, add watchlist

--- src/plot_history.py
+++ src/plot_history.py
@@ -3,66 +3,45 @@ from datetime import datetime, timedelta
 import plotly.graph_objects as go
 from botcoin.data.data_fetcher import HistoricalDataManager
 
+hdm = HistoricalDataManager(ticker="AAPL")
 
 # Create a naive datetime
-start = datetime(year=2025, month=4, day=16, hour=0, minute=0, second=0).replace(tzinfo=pytz.timezone("US/Eastern"))
-end = datetime(year=2025, month=4, day=16, hour=0, minute=0, second=0).replace(tzinfo=pytz.timezone("US/Eastern"))
+start   = datetime(year=2025, month=4, day=15, hour=0, minute=0, second=0)
+end     = datetime(year=2025, month=4, day=16, hour=0, minute=0, second=0)
 
-old_end = datetime.now(pytz.timezone("US/Eastern"))
-end = old_end - timedelta(days=2)  # Adjust to the previous day
-start = old_end - timedelta(days=27)
-print(f"Start: {start}")
-print(f"End: {end}")
+start   = hdm.tz.localize(start)
+end     = hdm.tz.localize(end)
 
-watchlist = [
-    "AAPL",
-    "META",
-    "GME",
-    "TSLA",
-    "MSTR",
-    "NVDA",
-    "NFLX",
-    "PLTR",
-    "COIN",
-    "AMD",
-]
+df = hdm.get_data(start=start, end=end)
 
-for ticker in watchlist:
-    print(f"Fetching data for {ticker}")
-    df = HistoricalDataManager(ticker=ticker).get_data(start=start, end=end)
-    print(f"Data for {ticker} fetched successfully")
-    print(len(df), " rows of data")
+# Plot candlestick chart
+fig = go.Figure(data=[go.Candlestick(
+    x=df.index,
+    open=df['Open'],
+    high=df['High'],
+    low=df['Low'],
+    close=df['Close'],
+    name="AAPL"
+)])
 
-# # Plot candlestick chart
-# fig = go.Figure(data=[go.Candlestick(
-#     x=df.index,
-#     open=df['Open'],
-#     high=df['High'],
-#     low=df['Low'],
-#     close=df['Close'],
-#     name="AAPL"
-# )])
+fig.update_layout(
+    title="AAPL 1-Min Candlestick Chart",
+    xaxis_title="Time",
+    yaxis_title="Price (USD)",
+    xaxis_rangeslider_visible=False,
+    template="plotly_white",
+    height=600
+)
 
-# fig.update_layout(
-#     title="AAPL 1-Min Candlestick Chart",
-#     xaxis_title="Time",
-#     yaxis_title="Price (USD)",
-#     xaxis_rangeslider_visible=False,
-#     template="plotly_white",
-#     height=600
-# )
+# Hide weekends and non-trading hours using range breaks
+fig.update_layout(
+    xaxis=dict(
+        rangeslider_visible=False,
+        rangebreaks=[
+            dict(bounds=["sat", "mon"]),  # Skip weekends
+            dict(bounds=[16, 9.5], pattern="hour")  # Skip outside market hours (4 PM - 9:30 AM)
+        ]
+    ),
+)
 
-# # Hide weekends and non-trading hours using range breaks
-# fig.update_layout(
-#     xaxis=dict(
-#         rangeslider_visible=False,
-#         rangebreaks=[
-#             dict(bounds=["sat", "mon"]),  # Skip weekends
-#             dict(bounds=[16, 9.5], pattern="hour")  # Skip outside market hours (4 PM - 9:30 AM)
-#         ]
-#     ),
-# )
-
-
-
-# fig.show()
+fig.show()
---
+++ src/watchlist.py
@@ -0,0 +1,12 @@
+tickers = [
+    "AAPL",
+    "META",
+    "GME",
+    "TSLA",
+    "MSTR",
+    "NVDA",
+    "NFLX",
+    "PLTR",
+    "COIN",
+    "AMD",
+]

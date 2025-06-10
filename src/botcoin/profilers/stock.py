357998d09d3b9327b476aae3947f0727cf58f6dd
Runfei Wu <47469275+JasonWurunfei@users.noreply.github.com>
2025-06-10 22:47:56 +0200

    Adding annual return to the profile

--- src/botcoin/profilers/stock.py
+++ src/botcoin/profilers/stock.py
@@ -26,6 +26,7 @@ class StockProfiler:
         start_date, end_date = self._get_date_range(timedelta(days=365 * 5))
         df_1d = self.dm.get_ohlcv_1d(symbol, start_date, end_date)
         quote = self.dm.dp.get_quote(symbol)
+        annual_returns = self.compute_annual_returns(df_1d)
         return {
             "symbol": symbol,
             "ipo_date": self.dm.dp.get_ipo_date(symbol),
@@ -33,6 +34,7 @@ class StockProfiler:
             "ohlcv_1min": df_1min,
             "1min_returns": self.compute_oc_returns(df_1min),
             "1d_returns": self.compute_oc_returns(df_1d),
+            "annual_return": annual_returns.mean(),
         }
 
     def compute_oc_returns(self, df: pd.DataFrame) -> pd.Series:
@@ -61,3 +63,32 @@ class StockProfiler:
         end_date = date.today()
         start_date = end_date - time_delta
         return (start_date, end_date)
+
+    def compute_annual_returns(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Compute annual returns for the given DataFrame. This
+        method calculates the annualized returns based on the daily returns.
+        It assumes that the DataFrame contains daily OHLCV data with a 'Close' column.
+
+        Assuming there is 252 trading days in a year, the annual returns can be
+        calculated as follows:
+
+        Annual Return = (p_x - p_x-252) / p_x-252
+        where:
+            - p_x is the closing price on day x
+            - p_x-252 is the closing price 252 days prior to day x
+
+        Args:
+            df (DataFrame): The DataFrame containing OHLCV data.
+        Returns:
+            Series: A Series with the computed annual returns.
+        """
+        shift_days = 252
+        shifted_close = df["Close"].shift(shift_days)
+
+        df["annual_returns"] = (df["Close"] - shifted_close) / shifted_close
+
+        # Remove NaN values that result from the shift operation
+        df = df.dropna(subset=["annual_returns"])
+
+        return df["annual_returns"]
